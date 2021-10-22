1599b206fceef69b492b1bfc675186edb10194bc
Joe Whelton <joe.whelton@mycit.ie>
2021-10-22 09:56:30 +0100

    Initial commit

---
+++ app/server.py
@@ -0,0 +1,24 @@
+import logging
+from concurrent import futures
+
+import grpc
+from spelling_bee_pb2 import GameResponse
+from spelling_bee_pb2_grpc import SpellingBeeServicer, add_SpellingBeeServicer_to_server
+
+
+class GameServer(SpellingBeeServicer):
+    def StartGame(self, request, context):
+        return GameResponse(userName="Game created for {}".format(request.userName))
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_SpellingBeeServicer_to_server(GameServer(), server)
+    server.add_insecure_port('[::]:50055')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
